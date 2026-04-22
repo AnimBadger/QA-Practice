94c13b570b3df224924cc35dd84c17dca25279ad
animbadger <anim.ansah.stephen@gmail.com>
2026-04-22 15:41:30 +0000

    removed test

--- tests/input-fields/input_fields.spec.ts
+++ tests/input-fields/input_fields.spec.ts
@@ -1,7 +1,7 @@
 // @ts-check
-import { test, expect, type Locator } from '@playwright/test';
+import { test, expect, type Locator } from "@playwright/test";
 
-test.describe('Input box practice', () => {
+test.describe("Input box practice", () => {
   let movieName: Locator;
   let appendTextField: Locator;
   let presentTextField: Locator;
@@ -10,83 +10,76 @@ test.describe('Input box practice', () => {
   let readonlyField: Locator;
 
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.qaplayground.com/practice/input-fields');
+    await page.goto("https://www.qaplayground.com/practice/input-fields");
 
-    movieName = page.getByTestId('input-movie-name');
-    appendTextField = page.getByTestId('input-append-text');
-    presentTextField = page.getByTestId('input-verify-text');
-    cleartextField = page.getByTestId('input-clear-text')
-    disabledField = page.getByTestId('input-disabled')
-    readonlyField = page.getByTestId('input-readonly')
+    movieName = page.getByTestId("input-movie-name");
+    appendTextField = page.getByTestId("input-append-text");
+    presentTextField = page.getByTestId("input-verify-text");
+    cleartextField = page.getByTestId("input-clear-text");
+    disabledField = page.getByTestId("input-disabled");
+    readonlyField = page.getByTestId("input-readonly");
   });
 
-  test.describe('Movie input field', () => {
-    test('accepts valid input', async () => {
-      const value = 'User123';
+  test.describe("Movie input field", () => {
+    test("accepts valid input", async () => {
+      const value = "User123";
       await movieName.fill(value);
       await expect(movieName).toHaveValue(value);
     });
 
-    test('has correct placeholder and accepts input', async () => {
+    test("has correct placeholder and accepts input", async () => {
       await expect(movieName).toHaveAttribute(
-        'placeholder',
-        'Enter hollywood movie name'
+        "placeholder",
+        "Enter hollywood movie name",
       );
 
-      const value = 'Inception';
+      const value = "Inception";
       await movieName.fill(value);
       await expect(movieName).toHaveValue(value);
     });
   });
 
-  test('appends text and moves focus on Tab', async () => {
-    await appendTextField.fill('I am goody yahh');
-    await appendTextField.press('Tab');
+  test("appends text and moves focus on Tab", async () => {
+    await appendTextField.fill("I am goody yahh");
+    await appendTextField.press("Tab");
 
     await expect(presentTextField).toBeFocused();
   });
 
-  test('should be able to append text', async () => {
+  test("should be able to append text", async () => {
     const existingValue = await appendTextField.inputValue();
 
-    const newText = '- dat';
+    const newText = "- dat";
     await appendTextField.fill(existingValue + newText);
-    expect(appendTextField).toHaveValue(existingValue + newText); 
-    });
-
-    test('verify text present inside input field matches expected value', async () => {
-        const fieldValue = await presentTextField.inputValue();
-        await expect(presentTextField).toHaveValue(fieldValue)
-    })
-
-    test('verify input field text can be cleared successfully', async () => {
-    await cleartextField.fill('');
-    await expect(cleartextField).toBeEmpty();
-});
-  test('verify disabled fields cannot be edited by user', async () => {
-    await expect(disabledField).toBeDisabled();
-    await expect(disabledField).toHaveValue('Enter');
-  })
-
-  test('verify is Enabled() returns false for disabled fields', async () => {
-    await expect(disabledField).toBeDisabled();
+    expect(appendTextField).toHaveValue(existingValue + newText);
   });
 
-  test('readonly field should not be editable', async () => {
-  await expect(readonlyField).toHaveAttribute('readonly', '');
-  await expect(readonlyField).toHaveValue('The text is readonly');
-});
+  test("verify text present inside input field matches expected value", async () => {
+    const fieldValue = await presentTextField.inputValue();
+    await expect(presentTextField).toHaveValue(fieldValue);
+  });
 
+  test("verify input field text can be cleared successfully", async () => {
+    await cleartextField.fill("");
+    await expect(cleartextField).toBeEmpty();
+  });
+  test("verify disabled fields cannot be edited by user", async () => {
+    await expect(disabledField).toBeDisabled();
+    await expect(disabledField).toHaveValue("Enter");
+  });
+
+  test("verify is Enabled() returns false for disabled fields", async () => {
+    await expect(disabledField).toBeDisabled();
+  });
 });
-  
 
 //   // 2. Boundary & Validation
 //   test('should respect max length attribute', async () => {
 //     const maxLength: number = 10;
 //     const longString: string = 'A'.repeat(maxLength + 5);
-    
+
 //     await input.fill(longString);
-    
+
 //     const value: string = await input.inputValue();
 //     expect(value.length).toBeLessThanOrEqual(maxLength);
 //   });
@@ -109,7 +102,7 @@ test.describe('Input box practice', () => {
 
 //   test('should trim leading and trailing whitespace', async () => {
 //     await input.fill('  trimmed  ');
-//     await input.blur(); 
+//     await input.blur();
 //     await expect(input).toHaveValue('trimmed');
 //   });
 
